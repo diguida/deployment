158358e56b3038d32719a081d7e3e311992cbfbe
Salvatore Di Guida <salvatore.di.guida@cern.ch>
2016-10-26 20:05:03 +0200

    Dropped unwanted layout files in wrong path, and fixed function names.

--- dqmgui/layouts/shift_castor_T0_layout.py
+++ dqmgui/layouts/shift_castor_T0_layout.py
@@ -1,38 +1,37 @@
-def castorlayout(i, p, *rows):
-    i["Castor/Layouts/" + p] = DQMItem(layout=rows)
+def shiftcastorlayout(i, p, *rows):
+    i["00 Shift/Castor/" + p] = DQMItem(layout=rows)
 
-
-castorlayout(dqmitems, "01 - Map of frontend and readout errors",
+shiftcastorlayout(dqmitems, "01 - Map of frontend and readout errors",
            [{ 'path': "Castor/CastorDigiMonitor/CASTOR QIE_capID+er+dv",
              'description':"Frontend and readout errors"}]
            )
 
-castorlayout(dqmitems, "02 - Channel-wise timing",
+shiftcastorlayout(dqmitems, "02 - Channel-wise timing",
            [{ 'path': "Castor/CastorDigiMonitor/QfC=f(x=Tile y=TS) (cumulative)",
              'description':"Channel-wise timing"}]
            )
-castorlayout(dqmitems, "02b - Channel-wise timing (rms)",
+shiftcastorlayout(dqmitems, "02b - Channel-wise timing (rms)",
            [{ 'path': "Castor/CastorDigiMonitor/QrmsfC=f(Tile TS)",
              'description':"Channel-wise timing (rms)"}]
            )
 
-castorlayout(dqmitems, "03 - CASTOR DeadChannelsMap",
+shiftcastorlayout(dqmitems, "03 - CASTOR DeadChannelsMap",
            [{ 'path': "Castor/CastorDigiMonitor/CASTOR DeadChannelsMap",
              'description':"CASTOR DeadChannelsMap"}]
            )
 
-castorlayout(dqmitems, "04 - DigiSize",
+shiftcastorlayout(dqmitems, "04 - DigiSize",
            [{ 'path': "Castor/CastorDigiMonitor/DigiSize",
              'description':"CASTOR DigiSize",
 		'draw': { 'ytype':'log' } }]
            )
 
-castorlayout(dqmitems, "05 - CASTOR Tower Depth",
+shiftcastorlayout(dqmitems, "05 - CASTOR Tower Depth",
            [{ 'path': "Castor/CastorRecHitMonitor/CASTORTowerDepth",
              'description':"CASTORTowerDepth"}]
            )
 
-castorlayout(dqmitems, "06 - Tower EM vs HAD",
+shiftcastorlayout(dqmitems, "06 - Tower EM vs HAD",
            [{ 'path': "Castor/CastorRecHitMonitor/CASTORTowerEMvsEhad",
              'description':"CASTOR Tower EM vs Ehad",
  'draw': { 'xtype': 'log', 'ytype':'log', 'drawopts': "COLZ" } }]
